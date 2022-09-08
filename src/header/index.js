1766fda7bc11bd9949fd0be64af972d254991495
Nausheen.Akhter <nausheen.akhter@ril.com>
2022-09-08 21:13:27 +0530

    added routing mechanism

--- src/header/index.js
+++ src/header/index.js
@@ -3,14 +3,17 @@ import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='header'>
-      <div className='header__logo'>
-        <StorefrontIcon className='header__logoImage' fontSize='large' />
-        <h2 className='header__logoTitle'>eComm</h2>
-      </div>
+      <Link to='/' style={{ textDecoration: 'none' }}>
+        <div className='header__logo'>
+          <StorefrontIcon className='header__logoImage' fontSize='large' />
+          <h2 className='header__logoTitle'>eComm</h2>
+        </div>
+      </Link>
       <div className='header__search'>
         <input type='text' className='header__searchInput' />
         <SearchIcon className='header__searchIcon' />
@@ -24,10 +27,12 @@ function Header() {
           <span className='nav__item_1'>Your</span>
           <span className='nav__item_2'>Shop</span>
         </div>
-        <div className='nav__item nav__basket'>
-          <ShoppingBasketIcon  className='itemBasket'/>
-          <span className='nav__item_2 nav__basketCount'>0</span>
-        </div>
+        <Link to={'/checkout'} style={{ textDecoration: 'none' }}>
+          <div className='nav__item nav__basket'>
+            <ShoppingBasketIcon className='itemBasket' />
+            <span className='nav__item_2 nav__basketCount'>0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
